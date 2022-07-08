dfc0cd2af1cf1c61eb37fcd14094e205cd5bfcab
guedesclaudio <cgsarmentosilva@gmail.com>
2022-07-08 16:03:03 -0300

    Ajustes tela de projetos

--- myscript.js
+++ myscript.js
@@ -3,13 +3,13 @@ const elementsOff = document.querySelectorAll(".off")
 
 
 function descobreTexto(element) {
-    element.querySelector("p").classList.add("escondido")
-    element.querySelector("h4").classList.remove("escondido")
+    element.querySelector("p").classList.add("hidden")
+    element.querySelector("h4").classList.remove("hidden")
 }
 
 function encobreTexto(element) {
-    element.querySelector("p").classList.remove("escondido")
-    element.querySelector("h4").classList.add("escondido")
+    element.querySelector("p").classList.remove("hidden")
+    element.querySelector("h4").classList.add("hidden")
 }
 
 setInterval(() => document.querySelector(".img-profile img").classList.toggle("move"), 300)
@@ -25,23 +25,23 @@ function stackOff(element) {
 
 function viewProjects() {
     const projects = document.querySelector(".projects")
-    projects.classList.toggle("escondido")
+    projects.classList.toggle("hidden")
     setTimeout(() => projects.classList.add("projects-animation"), 10)
     setTimeout(() => projects.classList.remove("projects-desanimation"), 10)
     
-    elementsOn[0].classList.toggle("escondido")
-    elementsOn[1].classList.toggle("escondido")
-    elementsOff[0].classList.toggle("escondido")
-    elementsOff[1].classList.toggle("escondido")
+    elementsOn[0].classList.toggle("hidden")
+    elementsOn[1].classList.toggle("hidden")
+    elementsOff[0].classList.toggle("hidden")
+    elementsOff[1].classList.toggle("hidden")
 }
 
 function notViewProjects() {
     const projects = document.querySelector(".projects")
-    setTimeout(() => projects.classList.toggle("escondido"), 200)
+    setTimeout(() => projects.classList.toggle("hidden"), 200)
     projects.classList.remove("projects-animation")
 
-    elementsOn[0].classList.toggle("escondido")
-    elementsOn[1].classList.toggle("escondido")
-    elementsOff[0].classList.toggle("escondido")
-    elementsOff[1].classList.toggle("escondido")
+    elementsOn[0].classList.toggle("hidden")
+    elementsOn[1].classList.toggle("hidden")
+    elementsOff[0].classList.toggle("hidden")
+    elementsOff[1].classList.toggle("hidden")
 }
