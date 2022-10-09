782065a758c3c809ef2f7cd6a1ea2a5150b4b6e5
guedesclaudio <cgsarmentosilva@gmail.com>
2022-10-09 18:44:25 -0300

    FIX : description corrected

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -12,7 +12,7 @@ export default function Panel() {
                 <SubTitle>Meu nome é Cláudio Guedes</SubTitle>
                 <Description>
                     Sou desenvolvedor full stack júnior.
-                    Tenho experiência com aplicações web (SPA e Api Rest) utilizando principalmente React, Node.js, MongoDB e PostgreSQL. 
+                    Tenho experiência com aplicações web (SPA, REST API, CRUD) utilizando principalmente React, Node.js, MongoDB e PostgreSQL. 
                 </Description>
                 <Description>
                     Estou concluindo o curso de Oceanografia na UERJ, e foi através da faculdade que surgiu a minha paixão por programação.
